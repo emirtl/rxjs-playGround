219277b490dba9ee31bd79ee04af280ffc6d7938
emirtl <tlemir55@gmail.com>
2022-04-28 10:46:13 +0300

    filter operator

---
+++ pipeableOperator.ts
@@ -0,0 +1,31 @@
+import { filter, Observable } from 'rxjs';
+
+// interface NewsItem {
+//   category: 'Business' | 'Sports';
+//   content: string;
+// }
+
+// const newsFeed$ = new Observable<NewsItem>((subscriber) => {
+//   setTimeout(
+//     () => subscriber.next({ category: 'Business', content: 'A' }),
+//     1000
+//   );
+//   setTimeout(() => subscriber.next({ category: 'Sports', content: 'B' }), 3000);
+//   setTimeout(
+//     () => subscriber.next({ category: 'Business', content: 'C' }),
+//     4000
+//   );
+//   setTimeout(() => subscriber.next({ category: 'Sports', content: 'D' }), 6000);
+//   setTimeout(
+//     () => subscriber.next({ category: 'Business', content: 'E' }),
+//     7000
+//   );
+// });
+
+// const sportsNewsFeed$ = newsFeed$.pipe(
+//   filter((item) => item.category === 'Sports')
+// );
+
+// newsFeed$.subscribe((item) => console.log(item));
+
+//__________________________________________________________________
