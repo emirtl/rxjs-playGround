81995dec8818eae351c0ea3964dc584551549e30
emirtl <tlemir55@gmail.com>
2022-04-29 10:58:39 +0300

    concatMap()

--- index.ts
+++ index.ts
@@ -1,3 +1,16 @@
 import './style.css';
 import { of, map, Observable } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { catchError, concatMap } from 'rxjs/operators';
+
+// const obs$ = new Observable((subscriber) => {
+//   setTimeout(() => {
+//     subscriber.next('A');
+//   }, 3000);
+
+//   setTimeout(() => {
+//     subscriber.next('B');
+//   }, 6000);
+// });
+// obs$
+//   .pipe(concatMap((value) => of(1000, 2000)))
+//   .subscribe((value) => console.log(value));
